16cf4d081c254f76d02b22cb1fdcda8897155836
Hentioe <meow.i5.br@gmail.com>
2023-05-11 07:03:20 +0800

    Update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Entitydown.MixProject do
   def project do
     [
       app: :entitydown,
-      version: "0.1.0",
+      version: "0.1.0-dev",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
