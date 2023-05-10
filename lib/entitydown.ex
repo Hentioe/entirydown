a01cf98d146a5bd6b069155642b56ae2d1b75370
Hentioe <meow.i5.br@gmail.com>
2023-05-11 06:52:48 +0800

    Add entry function

--- lib/entitydown.ex
+++ lib/entitydown.ex
@@ -1,3 +1,14 @@
 defmodule Entitydown do
   @moduledoc false
+
+  alias Entitydown.{Entity, Parser, Renderer}
+
+  @type markdown_text :: String.t()
+  @type plain_text :: String.t()
+  @type entities :: [Entity.t()]
+
+  @spec extract(markdown_text) :: {plain_text, entities}
+  def extract(markdown_text) do
+    markdown_text |> Parser.parse() |> Renderer.render()
+  end
 end
