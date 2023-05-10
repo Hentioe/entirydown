e683b97627a14c4e7f4c4f2ae44b6ab26e1b2ce0
Hentioe <meow.i5.br@gmail.com>
2023-05-11 06:26:35 +0800

    Fix typo

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Entirydown
+# Entitydown
 
 WIP
--- mix.exs
+++ mix.exs
@@ -1,9 +1,9 @@
-defmodule Entirydown.MixProject do
+defmodule Entitydown.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :entirydown,
+      app: :entitydown,
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
